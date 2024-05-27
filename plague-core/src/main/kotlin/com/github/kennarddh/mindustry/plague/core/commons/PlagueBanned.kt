386eadf3eb190cb803f248db38e5ce225d5ede34
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-05-27 13:48:20 +0700

    feat: Plague simple

---
+++ plague-core/src/main/kotlin/com/github/kennarddh/mindustry/plague/core/commons/PlagueBanned.kt
@@ -0,0 +1,241 @@
+package com.github.kennarddh.mindustry.plague.core.commons
+
+import kotlinx.coroutines.sync.withLock
+import mindustry.content.Blocks
+import mindustry.content.UnitTypes
+import mindustry.type.UnitType
+import mindustry.world.Block
+
+object PlagueBanned {
+    val serpuloUnitConstructorBlocks: Set<Block> = setOf(
+        Blocks.groundFactory,
+        Blocks.airFactory,
+        Blocks.navalFactory,
+        Blocks.additiveReconstructor,
+        Blocks.multiplicativeReconstructor,
+        Blocks.exponentialReconstructor,
+        Blocks.tetrativeReconstructor,
+    )
+
+    val erekirUnitConstructorBlocks: Set<Block> = setOf(
+        Blocks.shipAssembler,
+        Blocks.mechAssembler,
+        Blocks.tankAssembler,
+        Blocks.tankFabricator,
+        Blocks.shipFabricator,
+        Blocks.mechFabricator,
+        Blocks.tankRefabricator,
+        Blocks.mechRefabricator,
+        Blocks.shipRefabricator,
+        Blocks.primeRefabricator
+    )
+
+    val unitConstructorBlocks = serpuloUnitConstructorBlocks + erekirUnitConstructorBlocks
+
+    val survivorBannedBlocks: Set<Block> =
+        unitConstructorBlocks - setOf(Blocks.airFactory, Blocks.additiveReconstructor)
+
+    val plagueBannedBlocks: Set<Block> = unitConstructorBlocks -
+            setOf(Blocks.airFactory, Blocks.additiveReconstructor) +
+            setOf(
+                // Serpulo walls
+                Blocks.surgeWall,
+                Blocks.surgeWallLarge,
+                Blocks.thoriumWall,
+                Blocks.thoriumWallLarge,
+                Blocks.phaseWall,
+                Blocks.phaseWallLarge,
+                Blocks.titaniumWall,
+                Blocks.titaniumWallLarge,
+                Blocks.copperWallLarge,
+                Blocks.copperWall,
+                Blocks.door,
+                Blocks.doorLarge,
+                Blocks.plastaniumWall,
+                Blocks.plastaniumWallLarge,
+
+                // Erekir walls
+                Blocks.berylliumWall,
+                Blocks.berylliumWallLarge,
+                Blocks.tungstenWall,
+                Blocks.tungstenWallLarge,
+                Blocks.blastDoor,
+                Blocks.reinforcedSurgeWall,
+                Blocks.reinforcedSurgeWallLarge,
+                Blocks.carbideWall,
+                Blocks.carbideWallLarge,
+                Blocks.shieldedWall,
+
+                // Serpulo power generators
+                Blocks.combustionGenerator,
+                Blocks.thermalGenerator,
+                Blocks.steamGenerator,
+                Blocks.differentialGenerator,
+                Blocks.rtgGenerator,
+                Blocks.solarPanel,
+                Blocks.largeSolarPanel,
+                Blocks.thoriumReactor,
+                Blocks.impactReactor,
+
+                // Erekir power generators
+                Blocks.turbineCondenser,
+                Blocks.chemicalCombustionChamber,
+                Blocks.fluxReactor,
+                Blocks.neoplasiaReactor
+            )
+
+    val alwaysAllowedUnits: Set<UnitType> = setOf(
+        UnitTypes.mono,
+        UnitTypes.poly,
+        UnitTypes.flare
+    )
+
+    val serpuloGroundUnits: Set<UnitType> = setOf(
+        // Dagger tree
+        UnitTypes.dagger,
+        UnitTypes.mace,
+        UnitTypes.fortress,
+        UnitTypes.scepter,
+        UnitTypes.reign,
+
+        // Crawler tree
+        UnitTypes.crawler,
+        UnitTypes.atrax,
+        UnitTypes.spiroct,
+        UnitTypes.arkyid,
+        UnitTypes.toxopid,
+
+        // Nova tree
+        UnitTypes.nova,
+        UnitTypes.pulsar,
+        UnitTypes.quasar,
+        UnitTypes.vela,
+        UnitTypes.corvus,
+    )
+
+    val serpuloAirUnits: Set<UnitType> = setOf(
+        // Mono tree
+        UnitTypes.mono,
+        UnitTypes.poly,
+        UnitTypes.mega,
+        UnitTypes.quad,
+        UnitTypes.oct,
+
+        // Flare tree
+        UnitTypes.flare,
+        UnitTypes.horizon,
+        UnitTypes.zenith,
+        UnitTypes.antumbra,
+        UnitTypes.eclipse,
+    )
+
+    val serpuloNavalUnits: Set<UnitType> = setOf(
+        // Risso tree
+        UnitTypes.risso,
+        UnitTypes.minke,
+        UnitTypes.bryde,
+        UnitTypes.sei,
+        UnitTypes.omura,
+
+        // Retusa tree
+        UnitTypes.retusa,
+        UnitTypes.oxynoe,
+        UnitTypes.cyerce,
+        UnitTypes.aegires,
+        UnitTypes.navanax,
+    )
+
+    val serpuloUnits = serpuloGroundUnits + serpuloAirUnits + serpuloNavalUnits
+
+    val erekirTankUnits: Set<UnitType> = setOf(
+        UnitTypes.stell,
+        UnitTypes.locus,
+        UnitTypes.precept,
+        UnitTypes.vanquish,
+        UnitTypes.conquer,
+    )
+
+    val erekirShipUnits: Set<UnitType> = setOf(
+        UnitTypes.elude,
+        UnitTypes.avert,
+        UnitTypes.obviate,
+        UnitTypes.quell,
+        UnitTypes.disrupt,
+    )
+
+    val erekirMechUnits: Set<UnitType> = setOf(
+        UnitTypes.merui,
+        UnitTypes.cleroi,
+        UnitTypes.anthicus,
+        UnitTypes.tecta,
+        UnitTypes.collaris,
+    )
+
+    val erekirUnits = erekirTankUnits + erekirShipUnits + erekirMechUnits
+
+    val allUnits = serpuloUnits + erekirUnits
+
+    suspend fun getCurrentPlagueBannedUnits(inStateLock: Boolean = false): Set<UnitType> {
+        val innerMethod = { state: PlagueState ->
+            when (state) {
+                PlagueState.Prepare -> allUnits
+
+                PlagueState.PlayingFirstPhase -> allUnits - alwaysAllowedUnits - serpuloGroundUnits - serpuloNavalUnits - erekirTankUnits - erekirMechUnits
+
+                PlagueState.PlayingSecondPhase,
+                PlagueState.Ended -> allUnits - alwaysAllowedUnits - serpuloGroundUnits - serpuloNavalUnits - serpuloAirUnits - erekirTankUnits - erekirShipUnits - erekirMechUnits
+            }
+        }
+
+        return if (inStateLock) {
+            innerMethod(PlagueVars.state)
+        } else {
+            PlagueVars.stateLock.withLock {
+                innerMethod(PlagueVars.state)
+            }
+        }
+    }
+
+    suspend fun getCurrentSurvivorsBannedUnits(inStateLock: Boolean = false): Set<UnitType> {
+        val innerMethod = { state: PlagueState ->
+            when (state) {
+                PlagueState.Prepare -> allUnits
+
+                PlagueState.PlayingFirstPhase,
+                PlagueState.PlayingSecondPhase,
+                PlagueState.Ended -> allUnits - alwaysAllowedUnits
+            }
+        }
+
+        return if (inStateLock) {
+            innerMethod(PlagueVars.state)
+        } else {
+            PlagueVars.stateLock.withLock {
+                innerMethod(PlagueVars.state)
+            }
+        }
+    }
+
+
+    suspend fun getCurrentPlagueBannedBlocks(inStateLock: Boolean = false): Set<Block> {
+        val innerMethod = { state: PlagueState ->
+            when (state) {
+                PlagueState.Prepare -> plagueBannedBlocks
+
+                PlagueState.PlayingFirstPhase,
+                PlagueState.PlayingSecondPhase,
+                PlagueState.Ended -> plagueBannedBlocks - serpuloUnitConstructorBlocks
+            }
+        }
+
+        return if (inStateLock) {
+            innerMethod(PlagueVars.state)
+        } else {
+            PlagueVars.stateLock.withLock {
+                innerMethod(PlagueVars.state)
+            }
+        }
+    }
+
+    fun getCurrentSurvivorsBannedBlocks(): Set<Block> = survivorBannedBlocks
+}
--- plague-core/src/main/kotlin/com/github/kennarddh/mindustry/plague/core/handlers/PlagueHandler.kt
+++ plague-core/src/main/kotlin/com/github/kennarddh/mindustry/plague/core/handlers/PlagueHandler.kt
@@ -1,18 +1,82 @@
 package com.github.kennarddh.mindustry.plague.core.handlers
 
+import com.github.kennarddh.mindustry.genesis.core.commons.priority.Priority
+import com.github.kennarddh.mindustry.genesis.core.commons.runOnMindustryThread
+import com.github.kennarddh.mindustry.genesis.core.events.annotations.EventHandler
+import com.github.kennarddh.mindustry.genesis.core.filters.FilterType
+import com.github.kennarddh.mindustry.genesis.core.filters.annotations.Filter
 import com.github.kennarddh.mindustry.genesis.core.handlers.Handler
 import com.github.kennarddh.mindustry.genesis.core.timers.annotations.TimerTask
+import com.github.kennarddh.mindustry.plague.core.commons.PlagueBanned
 import com.github.kennarddh.mindustry.plague.core.commons.PlagueState
 import com.github.kennarddh.mindustry.plague.core.commons.PlagueVars
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.sync.withLock
 import mindustry.Vars
+import mindustry.content.Blocks
 import mindustry.core.NetServer
+import mindustry.game.EventType
 import mindustry.game.Team
+import mindustry.gen.Groups
+import mindustry.net.Administration
 
 class PlagueHandler : Handler {
+    @Filter(FilterType.Action, Priority.High)
+    suspend fun actionFilter(action: Administration.PlayerAction): Boolean {
+        PlagueVars.stateLock.withLock {
+            if (action.block != null && action.block == Blocks.powerSource) return false
+
+            if (action.unit == null) return true
+
+            if (action.unit.team() == Team.blue) return false
+
+            if (PlagueVars.state === PlagueState.Prepare) return false
+
+            if (action.unit.team() == Team.malis) {
+                if (action.block != null) {
+                    if (PlagueBanned.getCurrentPlagueBannedBlocks(true).contains(action.block)) {
+                        return false
+                    }
+                }
+            } else {
+                if (action.block != null) {
+                    if (PlagueBanned.getCurrentSurvivorsBannedBlocks().contains(action.block)) {
+                        return false
+                    }
+                }
+            }
+
+            return true
+        }
+    }
+
+    fun getNewEmptyTeam(): Team? {
+        return Team.all.find {
+            // Non default team and not active.
+            it.id > 5 && !it.active()
+        }
+    }
+
+    @EventHandler
+    suspend fun onTap(event: EventType.TapEvent) {
+        PlagueVars.stateLock.withLock {
+            if (PlagueVars.state != PlagueState.Prepare) return
+        }
+
+        runOnMindustryThread {
+            if (event.player.team() == Team.blue) {
+                val newTeam = getNewEmptyTeam()
+                    ?: return@runOnMindustryThread event.player.sendMessage("[scarlet]No available team.")
+
+                event.player.team(newTeam)
+
+                event.tile.setNet(Blocks.coreShard, newTeam, 0)
+            }
+        }
+    }
+
     override suspend fun onInit() {
-        Vars.netServer.assigner = NetServer.TeamAssigner { player, players ->
+        Vars.netServer.assigner = NetServer.TeamAssigner { _, _ ->
             runBlocking {
                 PlagueVars.stateLock.withLock {
                     if (PlagueVars.state == PlagueState.Prepare) Team.blue
@@ -27,6 +91,16 @@ class PlagueHandler : Handler {
     suspend fun onStart() {
         PlagueVars.stateLock.withLock {
             PlagueVars.state = PlagueState.PlayingFirstPhase
+        }
+
+        runOnMindustryThread {
+            // Move every no team player to plague team
+            Groups.player.filter { it.team() === Team.blue }.forEach {
+                it.team(Team.malis)
+            }
+
+            // Remove all blue core
+            Team.blue.cores().forEach { it.kill() }
         }
     }
 
