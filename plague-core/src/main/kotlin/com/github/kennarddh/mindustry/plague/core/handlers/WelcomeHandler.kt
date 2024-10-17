a4041741d4bf1aaacffb2d8edd80df482fd159d3
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-10-17 16:21:21 +0700

    fix: Remove not needed \n

--- plague-core/src/main/kotlin/com/github/kennarddh/mindustry/plague/core/handlers/WelcomeHandler.kt
+++ plague-core/src/main/kotlin/com/github/kennarddh/mindustry/plague/core/handlers/WelcomeHandler.kt
@@ -15,14 +15,14 @@ class WelcomeHandler : Handler {
     fun onPlayerJoin(event: EventType.PlayerJoin) {
         event.player.infoMessage(
             """
-            [maroon]=== [gold]QUICK START[] ===[]\n
-            This is a game of [forest]Plague[](default) vs [yellow]Survivor[]. Last one standing.\n
-            [maroon]=== [gold]GAME STAGES[] ===[]\n
+            [maroon]=== [gold]QUICK START[] ===[]
+            This is a game of [forest]Plague[](default) vs [yellow]Survivor[]. Last one standing.
+            [maroon]=== [gold]GAME STAGES[] ===[]
             [royal]Prepare state (2min)[]: Choose to be [yellow]Survivor[], or join a [yellow]Survivor[] team. Place any block away from plague core to become [yellow]Survivor[].
             [royal]First Phase (45min)[]: [forest]Plague[] make units, [yellow]Survivor[] make defense. Air units don't do damage.
             [royal]Second Phase (15min)[]: Air units do damage.
             [royal]Sudden Death[]: [yellow]Survivors[] won, but [forest]Plague[] multiplies in strength. [yellow]Survive[] as long as possible.
-            \n
+            
             Full Rules in discord. Use [accent]/discord[] command.
             """.trimIndent()
         )
